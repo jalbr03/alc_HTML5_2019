035b5635146d444867830fc332589ee44e11ac02
jonathan <jalbr03droid@gmail.com>
2019-01-23 10:29:06 -0700

    good stuff with java script

---
+++ adventureGame/game.js
@@ -0,0 +1,21 @@
+// ha ha we dun
+/*
+    alot
+*/
+
+//will tell the html what to do
+//document.write("<h3>AAHHH you will <em>die</em> to day!!!</h3>");
+
+//alert("Warning bad people are in your pc!!! Call 555-555-5555");
+
+//confirm("Do you want to call?");
+
+//prompt("Do you want to call now?(y,n)");
+
+//prompt("really?! now?(y,n)");
+
+var pc = prompt("what is your name?");
+
+confirm("So your name is "+pc+"?");
+
+confirm("AH that sucks!! "+pc);
