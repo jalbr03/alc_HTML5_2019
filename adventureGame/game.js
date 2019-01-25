394ac2935c293ff3d846ca89653046e413698d34
jonathan <jalbr03droid@gmail.com>
2019-01-25 10:29:50 -0700

    more txt!

--- adventureGame/game.js
+++ adventureGame/game.js
@@ -14,8 +14,73 @@
 
 //prompt("really?! now?(y,n)");
 
-var pc = prompt("what is your name?");
+//var pc = prompt("what is your name?");
 
-confirm("So your name is "+pc+"?");
+//confirm("So your name is " + pc + "?");
 
-confirm("AH that sucks!! "+pc);
+//confirm("AH that sucks!! " + pc);
+
+game();
+
+function game(){
+    
+    document.write("Legend of my mouth");
+    var playerName = prompt("waht is your name?");
+    alert("welcome to the mouth! " + playerName);
+    
+    prison();w
+    
+    function prison(){
+        var prison = prompt("You wake up...Your head is puonding and vision blurred. After pacing around walking off the headache your vision clears. It appears you are in prison and you cannot remember why. /n -look around /n - go back to sleep /n - taunt the guard").toLowerCase();
+        
+        if(prison == "look around" || prison == "look"){
+            var prisonLook = prompt("The prison is small, dirty and damp. A rat scurries into a hole in the corner, Bugs squirm and scamper underfoot. There is a barred window to the back. an occupied bed a stranger asleep anoring to the right. The cell bars and locked door to the front. An empty bed whitch you had sleeped in the night before to the right. In the center of the dirt floor is lumpy moldy rug. /n -wake stranger /n move rug /n -eat bugs");
+        }
+    
+        else if(prison == "go back to sleep" || prison == "sleep" || "go to bed"){
+            alert("You fall back to sleep in your dirty cot. You dream of distant lands and adventures");
+            var resume = confirm ("do you wish to continue?");
+            
+        if(prison == "null"){
+            prison();
+        }
+            
+            if(resume == true){
+                prison();
+            }
+            else{
+                alert("game Over!");
+            }
+        }
+    } 
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
