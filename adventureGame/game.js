f2ecdad507b13038b5081bee9c41316eb67b60c1
jonathan <jalbr03droid@gmail.com>
2019-01-29 10:28:11 -0700

    nothing it sucks

--- adventureGame/game.js
+++ adventureGame/game.js
@@ -28,19 +28,26 @@ function game(){
     var playerName = prompt("waht is your name?");
     alert("welcome to the mouth! " + playerName);
     
-    prison();w
+    prison();
     
-    function prison(){
-        var prison = prompt("You wake up...Your head is puonding and vision blurred. After pacing around walking off the headache your vision clears. It appears you are in prison and you cannot remember why. /n -look around /n - go back to sleep /n - taunt the guard").toLowerCase();
+        function prison(){
+        var prison = prompt("You wake up...Your head is puonding and vision blurred. After pacing around walking off the headache your vision clears. It appears you are in prison and you cannot remember why. \n -look around \n - go back to sleep \n - taunt the guard").toLowerCase();
+       
         
         if(prison == "look around" || prison == "look"){
-            var prisonLook = prompt("The prison is small, dirty and damp. A rat scurries into a hole in the corner, Bugs squirm and scamper underfoot. There is a barred window to the back. an occupied bed a stranger asleep anoring to the right. The cell bars and locked door to the front. An empty bed whitch you had sleeped in the night before to the right. In the center of the dirt floor is lumpy moldy rug. /n -wake stranger /n move rug /n -eat bugs");
+            var prisonLook = prompt("The prison is small, dirty and damp. A rat scurries into a hole in the corner, Bugs squirm and scamper underfoot. There is a barred window to the back. an occupied bed a stranger asleep anoring to the right. The cell bars and locked door to the front. An empty bed whitch you had sleeped in the night before to the right. In the center of the dirt floor is lumpy moldy rug. \n -wake stranger \n -move rug \n -eat bugs").toLowerCase();
         }
     
         else if(prison == "go back to sleep" || prison == "sleep" || "go to bed"){
             alert("You fall back to sleep in your dirty cot. You dream of distant lands and adventures");
-            var resume = confirm ("do you wish to continue?");
+            //var resume = confirm ("do you wish to continue?").toLowerCase();
+        }
             
+         
+        else if(prison == "taunt" || prison == "taunt the guard" || "taunt guard"){
+            alert("you taunt the guard but does not move you make a sound but he is still standing as stone, you cant even see him breath").toLowerCase();
+        }
+        
         if(prison == "null"){
             prison();
         }
@@ -53,7 +60,7 @@ function game(){
             }
         }
     } 
-}
+
     
     
     
