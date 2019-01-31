2d2a5e329e075be6c5f7c5edcb86efab1efe6a67
jonathan <jalbr03droid@gmail.com>
2019-01-31 10:30:23 -0700

    It works!!

--- adventureGame/game.js
+++ adventureGame/game.js
@@ -28,39 +28,81 @@ function game(){
     var playerName = prompt("waht is your name?");
     alert("welcome to the mouth! " + playerName);
     
-    prison();
+    //Prison();
+    ThePrison();
     
-        function prison(){
-        var prison = prompt("You wake up...Your head is puonding and vision blurred. After pacing around walking off the headache your vision clears. It appears you are in prison and you cannot remember why. \n -look around \n - go back to sleep \n - taunt the guard").toLowerCase();
-       
         
-        if(prison == "look around" || prison == "look"){
-            var prisonLook = prompt("The prison is small, dirty and damp. A rat scurries into a hole in the corner, Bugs squirm and scamper underfoot. There is a barred window to the back. an occupied bed a stranger asleep anoring to the right. The cell bars and locked door to the front. An empty bed whitch you had sleeped in the night before to the right. In the center of the dirt floor is lumpy moldy rug. \n -wake stranger \n -move rug \n -eat bugs").toLowerCase();
-        }
-    
-        else if(prison == "go back to sleep" || prison == "sleep" || "go to bed"){
-            alert("You fall back to sleep in your dirty cot. You dream of distant lands and adventures");
-            //var resume = confirm ("do you wish to continue?").toLowerCase();
-        }
-            
-         
-        else if(prison == "taunt" || prison == "taunt the guard" || "taunt guard"){
-            alert("you taunt the guard but does not move you make a sound but he is still standing as stone, you cant even see him breath").toLowerCase();
-        }
-        
-        if(prison == "null"){
-            prison();
-        }
-            
-            if(resume == true){
-                prison();
-            }
-            else{
-                alert("game Over!");
-            }
-        }
     } 
 
+
+    function ThePrison(){
+        var thePrison = prompt("You wake up and see a man in the same bed you are in you get up not knowing why you are in this place. \n -wake man \n -taunt guard");
+        
+        if(thePrison == "wake man" ||  thePrison == "wake"){
+            var man = prompt("The man looks at you puzzled why you disturbed his slumber. \n -say sorry \n -punch him in the face");
+            
+            if(man == "say sorry")
+                {
+                    alert("the man says its fine I'm just...(snor)");
+                    
+                }
+            if(man == "punch" || "punch him" || "punch him in the face")
+                {
+                    alert("The sudden hit to the face knocks him out and gave him a bloody nose.");
+                    
+                }
+            StandingInThePrison();
+        }
+        
+        if(thePrison == "taunt" || thePrison == "taunt guard"){
+            var taunt = prompt("how do you want to taunt \n -spit \n -yell")
+            
+            if(taunt == "spit"){
+                
+                alert("you spit as hard as you can but just miss but he does not move you cant even see him breath");
+               }
+            if(taunt == "yell"){
+                
+                alert("you yell as loud as you can but he does not move you cant even see him breath");
+               }
+            StandingInThePrison();
+        }
+            
+        
+        else{
+            alert("What?!");
+            ThePrison();
+        }
+    function StandingInThePrison(){
+        var standingInThePrison = prompt("You are in the midle of the room \n -look \n -sleep \n -kill the man in bed ");
+        
+        if(standingInThePrison == "look"){
+            var looking = prompt("you see some mold, the man and a window. \n -kill the man in bed \n -eat mold \n -break window ")
+            
+            
+        } 
+            
+        if(standingInThePrison == "sleep"){
+            alert("You fall asleep in the cot.");
+            ThePrison();
+            
+            
+        }
+        
+        if(standingInThePrison == "kill the man in bed" || standingInThePrison == "kill"){
+            
+            
+        }
+            
+           
+            
+    }
+    }
+        
+        
+        
+        
+    
     
     
     
